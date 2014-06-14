d7d9479e30b5a5e8b94ee09c777bd172bc75565c
Anand Srinivasan <anandtech1532@gmail.com>
2014-06-13 23:23:41 -0400

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("HYC");
+    case mBTC: return QString("mHYC");
+    case uBTC: return QString::fromUtf8("μHYC");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("HideYoCoins");
+    case mBTC: return QString("Milli-HideYoCoins (1 / 1,000)");
+    case uBTC: return QString("Micro-HideYoCoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
